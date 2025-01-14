532d5f5807d2241b5200df4b48b0ba6a1ff69458
SamahAbdAlrahman <s11941150@stu.najah.edu>
2025-01-14 02:24:39 +0200

    login with jwt

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { UserModel } from "../../../DB/models/users.js"; 
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 // CURD
@@ -10,8 +11,7 @@ router.post('/users', async (req, res) => {
     const { name, email, password } = req.body;
     try {
       const hashPassword = bcrypt.hashSync(password, 8);
-  
-      const newUser = await UserModel.create({ name, email, password: hashPassword });
+        const newUser = await UserModel.create({ name, email, password: hashPassword });
   
       res.status(201).json(newUser);
     } catch (error) {
@@ -30,6 +30,32 @@ router.post('/users', async (req, res) => {
       res.status(500).json({ error: 'Failed' });
     }
   });
+  // login
+  router.post('/login',async(req,res)=>{
+    try {
+
+    const {email,password} = req.body;
+    const user = await UserModel.findOne({
+    where:{email:email}
+    });
+    if(user == null){
+    return res.status(404).json({message:"invaid email"});
+    }
+    const check = await bcrypt.compareSync(password,user.password);
+    if(check == false){
+    return res.status(400).json({message:"invalid password"});
+    }
+
+    // jwt token
+    const token = jwt.sign({id:user.id,name:user.name},"secret_key")
+    return res.status(200).json({message:"success",token});
+  }
+  catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+  });
+    
   // Update
   router.put('/users/:id', async (req, res) => {
     try {
